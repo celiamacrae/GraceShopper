8e710bc778e557b2db1b11e909e06a797efa1b05
lianach <liana.andreea97@yhaoo.com>
2020-01-14 18:05:49 -0500

    feature: added user profile

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,16 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-
+import {Link, Route} from 'react-router-dom'
+import UpdateUser from './update-user'
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const {email, firstName, lastName, address, imageURL} = props
 
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
+    <div className="profiles">
+      <div className="cards">
+        <div>
+          <div className="profile ">
+            <img src={imageURL} height="300px" width="300px" />
+            <h3 className="price">Welcome, {firstName + ' ' + lastName}!</h3>
+          </div>
+
+          <ul className="menu">
+            <li className="menu_item">
+              <Link to="/user">Profile</Link>
+            </li>
+            <li className="menu_item">
+              <Link to="/products">Order history</Link>
+            </li>
+            <li className="menu_item">
+              <Link to="/user/settings">Settings</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="profile_option">
+          {props.location.pathname === '/user' ? (
+            <div>
+              <div className="card_content">
+                <h3>First Name: {firstName}</h3>
+                <h3>Last Name: {lastName}</h3>
+                <h3>Address: {address}</h3>
+                <h3>Email: {email}</h3>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <Route
+                path={`${props.match.path}/settings`}
+                render={() => <UpdateUser />}
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
@@ -20,7 +59,11 @@ export const UserHome = props => {
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    email: state.user.email,
+    firstName: state.user.firstName,
+    lastName: state.user.lastName,
+    address: state.user.address,
+    imageURL: state.user.imageURL
   }
 }
 
--- seed.js
+++ seed.js
@@ -12,21 +12,27 @@ const users = [
     lastName: 'Chan',
     address: '123 Magnolia Ave.,NY 11206',
     email: 'liana.andreea97@yahoo.com',
-    password: '123'
+    password: '123',
+    imageURL:
+      'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
   },
   {
     firstName: 'Oscar',
     lastName: 'Chan',
     address: '123 Magnolia Ave.,NY 11206',
     email: 'oscar_19@yahoo.com',
-    password: '123'
+    password: '123',
+    imageURL:
+      'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
   },
   {
     firstName: 'Celia',
     lastName: 'Macrae',
     address: '309 E 52nd St., New York, NY 10022',
     email: 'celiamacrae@gmail.com',
-    password: '123'
+    password: '123',
+    imageURL:
+      'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
   }
 ]
 const products = [
@@ -59,7 +65,6 @@ const products = [
     category: 'meat'
   },
   {
-
     name: 'Eggs',
     price: 3.29,
     imgSrc:
@@ -72,6 +77,41 @@ const products = [
     imgSrc:
       'https://www.myemarket.in/image/cache/data/Vegetables/fresh-onion-red-v-1-kg-1-600x600.png',
     category: 'Vegetable'
+  },
+  {
+    name: 'Garlic',
+    price: 1.99,
+    imgSrc:
+      'https://www.myemarket.in/image/cache/data/Vegetables/Garlic-600x600.jpg',
+    category: 'Vegetable'
+  },
+  {
+    name: 'Potato',
+    price: 2.99,
+    imgSrc:
+      'https://www.myemarket.in/image/cache/data/Vegetables/Potato1-600x600.jpg',
+    category: 'Vegetable'
+  },
+  {
+    name: 'Beet Root',
+    price: 1.99,
+    imgSrc:
+      'https://www.myemarket.in/image/cache/data/Vegetables/fresh-cut-beetroot-600x600.gif',
+    category: 'Big_Vegetable'
+  },
+  {
+    name: 'Sweet Corn',
+    price: 0.99,
+    imgSrc:
+      'https://www.myemarket.in/image/cache/data/Vegetables/sweet%20corn-600x600.jpg',
+    category: 'Big_Vegetable'
+  },
+  {
+    name: 'Sweet Potato',
+    price: 1.99,
+    imgSrc:
+      'https://www.myemarket.in/image/cache/data/Vegetables/Beauregard-Sweet-Potato_0-600x600.png',
+    category: 'Big_Vegetable'
   }
 ]
 const orders = [
@@ -100,7 +140,6 @@ const orders = [
     paymentInformation: '1234 5678 9012 3456',
     email: 'celia.macrae@gmail.com',
     productId: 4
-
   }
 ]
 const productOrder = [
@@ -122,8 +161,7 @@ const seed = () =>
   Promise.all(users.map(user => User.create(user))).then(() =>
     Promise.all(products.map(pr => Product.create(pr))).then(
       () =>
-        Promise.all(orders.map(order => Order.create(order)))
-        .then(() =>
+        Promise.all(orders.map(order => Order.create(order))).then(() =>
           Promise.all(productOrder.map(po => ProductOrder.create(po)))
         )
 
