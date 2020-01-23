9256f419a4c541a6e3e6b0e89eb0a9f1beba77d2
lianach <liana.andreea97@yhaoo.com>
2020-01-23 00:08:25 -0500

    added tests for api routes

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -4,7 +4,7 @@ const {expect} = require('chai')
 
 const db = require('../db')
 const app = require('../index')
-const {Product, User} = require('../db/models')
+const {Product, User, Recipe, RecipeProduct} = require('../db/models')
 const agent = require('supertest')(app)
 describe('User routes', () => {
   beforeEach(() => {
@@ -25,12 +25,12 @@ describe('User routes', () => {
       },
       {
         id: 2,
-        name: 'Mushroom',
+        name: 'Mango',
         price: 2.99,
         weight: '0.5oz',
         imgSrc:
           'https://www.myemarket.in/image/cache/data/Vegetables/MUSHRUMS-600x600.jpg',
-        category: 'Vegetable',
+        category: 'Fruit',
         stockQuantity: 200
       }
     ]
@@ -50,67 +50,88 @@ describe('User routes', () => {
       const res = await agent.get('/api/products/2').expect(200)
 
       expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('Mushroom')
+      expect(res.body.name).to.be.equal('Mango')
     })
     it('PUT /api/delete/:id', async () => {
       await agent.delete('/api/products/2').expect(200)
       const allProducts = await agent.get('/api/products')
       expect(allProducts.body).to.have.length(1)
     })
-  })
-})
-
-describe('User Routes', () => {
-  const users = [
-    {
-      id: 1,
-      firstName: 'Liana',
-      status: 'admin',
-      lastName: 'Chan',
-      address: '123 Magnolia Ave.,NY 11206',
-      email: 'liana.andreea97@yahoo.com',
-      password: '123',
-      imageURL:
-        'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
-    },
-    {
-      id: 2,
-      firstName: 'Celia',
-      lastName: 'Macrae',
-      status: 'user',
-      address: '309 E 52nd St., New York, NY 10022',
-      email: 'celiamacrae@gmail.com',
-      password: '123',
-      imageURL:
-        'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
-    }
-  ]
-
-  beforeEach(async () => {
-    await User.bulkCreate(users)
+    it('CATEGORY /api/category', async () => {
+      const response = await agent.get('/api/products/filter/Fruit')
+      expect(response.body).to.be.an('array')
+      expect(response.body[0].name).to.be.equal('Mango')
+    })
   })
 
-  it('GET /api/users', async () => {
-    const res = await agent.get('/api/users/').expect(200)
+  describe('User Routes', () => {
+    const users = [
+      {
+        firstName: 'Liana',
+        status: 'admin',
+        lastName: 'Chan',
+        address: '123 Magnolia Ave.,NY 11206',
+        email: 'liana.andreea97@yahoo.com',
+        password: '123',
+        imageURL:
+          'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
+      },
+      {
+        firstName: 'Celia',
+        lastName: 'Macrae',
+        status: 'user',
+        address: '309 E 52nd St., New York, NY 10022',
+        email: 'celiamacrae@gmail.com',
+        password: '123',
+        imageURL:
+          'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/107/profiles/2394/profileImage/avatar-new400.jpg'
+      }
+    ]
 
-    expect(res.body).to.be.an('array')
-    expect(res.body).to.have.length(2)
-    expect(res.body[0].firstName).to.be.equal('Liana')
+    beforeEach(async () => {
+      await User.bulkCreate(users)
+    })
+
+    it('GET /api/users', async () => {
+      const res = await agent.get('/api/users/').expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body).to.have.length(2)
+      expect(res.body[0].firstName).to.be.equal('Liana')
+    })
+    it('GET /api/users/:id', async () => {
+      const res = await agent.get('/api/users/2').expect(200)
+
+      expect(res.body.firstName).to.be.equal('Celia')
+    })
+    it('DELETE /api/users/:id', async () => {
+      await agent.delete('/api/users/1').expect(204)
+      const allProducts = await agent.get('/api/users')
+      expect(allProducts.body).to.have.length(1)
+    })
   })
-  // it('GET /api/users/:id', async () => {
-  //   const res = await agent.put('/api/users/1',{lastName:'CHAN'}).expect(200)
 
-  //   expect(res.body.lastName).to.be.equal('CHAN')
-  // })
-  // it('PUT /api/users/:id', async () => {
-  //   const res = await agent.put('/api/users/1',{firstName:'Andreea'}).expect(200)
+  describe('Recepies routes', () => {
+    const recipies = [
+      {
+        id: 1,
+        name: 'Onion and mushrooms omelette',
+        description:
+          'Crack the eggs into a bowl and add a pinch of salt. Whisk until well beaten, then set aside.',
+        imageURL:
+          'https://www.seriouseats.com/recipes/images/2016/04/20160418-american-omelet-ham-and-cheese-21-1500x1125.jpg'
+      }
+    ]
 
-  //   expect(res.body).to.be.an('object')
-  //   expect(res.body.firstName).to.be.equal('Andreea')
-  // })
-  // it('DELETE /api/users/:id', async () => {
-  //   await agent.delete('/api/users/1').expect(204)
-  //   const  allProducts=await agent.get('/api/users');
-  //   expect(allProducts.body).to.have.length(1);
-  // })
+    beforeEach(async () => {
+      await Recipe.bulkCreate(recipies)
+    })
+
+    it('GET /api/recipies', async () => {
+      const res = await agent.get('/api/recipies').expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].name).to.be.equal('Onion and mushrooms omelette')
+    })
+  })
 })
